3ea396029b0fbb8a9585ef728719232b25044504
Fijon <huiqiangxu@126.com>
2022-02-02 11:52:01 +0800

    修改启动路径

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
-if [ ! -d "musicApp" ]; then
-  mkdir musicApp
+appPath=${HOME}/app/
+musicAppPath=${appPath}/musicApp
+stockAppPath=${appPath}/stockApp
+if [ ! -d ${musicAppPath} ]; then
+  mkdir -p ${musicApp}
 fi
-if [ ! -d "stockApp" ]; then
-  mkdir stockApp
+if [ ! -d ${stockAppPath} ]; then
+  mkdir -p ${stockAppPath}
 fi
-rm -rf ./musicApp/*
-rm -rf ./stockApp/*
+rm -rf ${musicAppPath}/*
+rm -rf ${stockAppPath}/*
 mvn clean package -Dmaven.test.skip=true
-cp music/target/*.jar ./musicApp/
-cp -r music/target/lib ./musicApp/
-cp stock/target/*.jar ./stockApp
-cp -r stock/target/lib ./stockApp
+cp music/target/*.jar ${musicAppPath}
+cp -r music/target/lib ${musicAppPath}
+cp stock/target/*.jar ${stockAppPath}
+cp -r stock/target/lib ${stockAppPath}
