224ddc426a98e5e7d99192bedfbd86b3ae3f3902
Fijon <huiqiangxu@126.com>
2022-02-13 11:56:41 +0800

    新增任务启动参数

--- build.sh
+++ build.sh
@@ -1,16 +1,23 @@
 appPath=${HOME}/app
 musicAppPath=${appPath}/musicApp
 stockAppPath=${appPath}/stockApp
+taskPath==${appPath}/task
 if [ ! -d ${musicAppPath} ]; then
   mkdir -p ${musicAppPath}
 fi
 if [ ! -d ${stockAppPath} ]; then
   mkdir -p ${stockAppPath}
 fi
+if [ ! -d ${taskPath} ]; then
+  mkdir -p ${taskPath}
+fi
 rm -rf ${musicAppPath}/*
 rm -rf ${stockAppPath}/*
+rm -rf ${taskPath}/*
 mvn clean package -Dmaven.test.skip=true
 cp music/target/*.jar ${musicAppPath}
 cp -r music/target/lib ${musicAppPath}
 cp stock/target/*.jar ${stockAppPath}
 cp -r stock/target/lib ${stockAppPath}
+cp task/target/*.jar ${taskPath}
+cp -r task/target/lib ${taskPath}
